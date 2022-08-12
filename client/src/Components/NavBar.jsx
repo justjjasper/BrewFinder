5fc2fad0a10edf196f8d76435db64d3507c5abac
JASPER <jasjasper101@gmail.com>
2022-08-12 14:21:54 -0700

    feat: Added a side bar display

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -39,13 +39,32 @@ export default function NavBar ( {username} ) {
       </NavBarContainer>
 
       <SideContainer>
+        <HomeIcon className="fa-solid fa-house"/>
         <Home onClick = { () => setToggleUserPage(false) }> Home </Home>
+
+
+        <GlobeIcon className="fa-solid fa-earth-americas"></GlobeIcon>
         <Public> Public </Public>
         <SideContent>
+          <SearchIcon className="fa-solid fa-magnifying-glass"/>
           <Search> Search </Search>
+          <TagsIcon className="fa-solid fa-tag"/>
           <Tags> Tags </Tags>
+          <CommunityIcon className ="fa-solid fa-people-group"/>
           <Community> Community </Community>
+          <CompanyIcon className="fa-solid fa-briefcase"/>
           <Companies> Companies </Companies>
+
+          <FormsContainer>
+            <FormHeader> Forms </FormHeader>
+            <FormContent>
+              <FormList> How to ferment beer </FormList>
+              <FormList> What is hoppy beer </FormList>
+              <FormList> What makes beer extra vs lite</FormList>
+              <FormList> The best type of beer for micheladas </FormList>
+              <FormList> MODELO TIMEE </FormList>
+            </FormContent>
+          </FormsContainer>
         </SideContent>
       </SideContainer>
       {!toggleUserPage && <Content addToFaves={addToFaves}/>}
@@ -70,36 +89,101 @@ var NavBarContainer = styled.div`
   border-radius: 10px;
 `;
 
+var FormHeader = styled.span`
+  position: relative;
+  left: 80;
+`;
+
+var FormsContainer = styled.div`
+  border: 1px solid;
+  width: 225px;
+  height: 300px;
+  position: relative;
+  left: -60;
+  bottom: 95;
+  display: flex;
+  flex-direction: column;
+  border-radius: 10px;
+`;
+
+var FormContent = styled.div`
+  padding: 5px;
+`;
+
+var FormList = styled.li`
+  font-size: 20px;
+`;
+
 var Search = styled.span`
   margin: 5px;
+  position: relative;
+  right: 17.7;
+  bottom: 14;
   :hover {
     cursor: pointer;
     font-weight: bold;
   };
 `;
 
-var Tags = styled.span`
-margin: 5px;
-:hover {
-  cursor: pointer;
-  font-weight: bold;
-};
-`;
-
-var Community= styled.span`
-margin: 5px;
-:hover {
-  cursor: pointer;
-  font-weight: bold;
-};
+var SearchIcon = styled.i`
+  position: relative;
+  font-size: 22;
+  right: 37;
+  top: 15;
+  color: #1976d2;
 `;
 
 var Companies = styled.span`
 margin: 5px;
+position: relative;
+bottom: 120;
+left: -12;
 :hover {
   cursor: pointer;
   font-weight: bold;
 };
+`;
+
+var CompanyIcon = styled.i`
+  font-size: 23;
+  position: relative;
+  left: -37;
+  bottom: 89;
+  color: #1976d2;
+`;
+
+var HomeIcon = styled.i`
+  position: relative;
+  left: 33;
+  top: 16;
+  font-size: 20;
+  color: #1976d2;
+`;
+
+var GlobeIcon = styled.i`
+  position: relative;
+  font-size: 20;
+  right: 62;
+  top: 55;
+  color: #1976d2;
+`;
+
+var Community= styled.span`
+  margin: 5px;
+  position: relative;
+  left: -12;
+  bottom: 85;
+  :hover {
+    cursor: pointer;
+    font-weight: bold;
+  };
+`;
+
+var CommunityIcon = styled.i`
+  position: relative;
+  left: -40;
+  bottom: 54;
+  color: #1976d2;
 `;
 
 var BrewFinderHome = styled.span`
@@ -122,6 +206,24 @@ var UserIcon = styled.i`
   }
 `;
 
+var Tags = styled.span`
+  margin: 5px;
+  position: relative;
+  bottom: 50px;
+  right: 19;
+  :hover {
+    cursor: pointer;
+    font-weight: bold;
+  };
+`;
+
+var TagsIcon = styled.i`
+  position: relative;
+  bottom: 17;
+  left: -35;
+  color: #1976d2;
+`;
+
 var Home = styled.span`
   font-size: 25px;
   position: relative;
@@ -129,15 +231,19 @@ var Home = styled.span`
   top: 20px;
   :hover {
     cursor: pointer;
-    color: #1976d2;
-  }
+    font-weight: bold;
+  };
 `;
 
 var Public = styled.span`
   font-size: 25px;
   position: relative;
-  right: 37px;
+  right: 62px;
   top: 55px;
+  :hover {
+    cursor: pointer;
+    font-weight: bold;
+  };
 `;
 
 var SideContent = styled.div`
